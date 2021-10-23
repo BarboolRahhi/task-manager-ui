8ce2dae19ca99dc63b2afb2e08d64427938bc993
rahhibarbool <rahhi60@gmail.com>
2021-10-23 23:36:39 +0530

    added login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
